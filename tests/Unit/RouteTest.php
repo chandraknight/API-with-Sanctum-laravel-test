ec5959d388c2e059ddd365fca4989a4ecaa22447
Chandra Ghimire <ghimire.chandra84@gmail.com>
2021-04-29 12:24:07 +0545

    route simple test added

---
+++ tests/Unit/RouteTest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+
+class RouteTest extends TestCase
+{
+    /**
+     * A basic unit test example.
+     *
+     * @return void
+     */
+    public function test_a_basic_request()
+    {
+        $response = $this->get('/');
+
+        $response->assertStatus(200);
+    }
+}
